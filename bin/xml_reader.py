119546201c695299f9e40b80438e0c1aabae8adb
ceIia <me@celiabourial.com>
2019-08-20 15:59:04 +0200

    moved imports inside the function

--- bin/xml_reader.py
+++ bin/xml_reader.py
@@ -1,7 +1,10 @@
-import xml.etree.ElementTree as ET
-
 def read_xml_file(file_path):
+  import xml.etree.ElementTree as ET
+  import os 
+  
   tree = ET.parse(file_path)
   root = tree.getroot()
+  
+  os.remove(file_path)
   
   return root
