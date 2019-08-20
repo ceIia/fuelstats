8c2e5753f94cc6ffcd2ba58694b8ef4c319f9114
ceIia <me@celiabourial.com>
2019-08-20 15:55:44 +0200

    replaced wget by boto to get data file from aws s3

--- bin/archive_downloader.py
+++ bin/archive_downloader.py
@@ -1,15 +1,25 @@
-import wget
+def download_archive(file_name, bucket_name): 
+  import boto3, botocore, random, string, os
 
-def download_archive(file_name, bucket_name):
-  # build url
+  s3 = boto3.resource('s3')
   
-  url = "https://" + bucket_name + ".s3.eu-west-3.amazonaws.com/" + file_name + ".xml"
-  print(url)
+  def randomStringDigits(stringLength=6):
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
   
-  # download the xml data archive
-  fileName = wget.download(url, '/tmp')
+  archive_name = '/tmp/' + randomStringDigits(8) + '_' + os.getenv("DATA_ARCHIVE_NAME") + '_archive.xml'
+  target_file_name = os.getenv("DATA_ARCHIVE_NAME") + '.xml'
   
-  filePath = fileName
-
+  # download file from aws s3 somehow
+  try:
+    s3.Bucket(os.getenv("S3_BUCKET_NAME")).download_file(target_file_name, archive_name)
+  except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+      print("The S3 object you requested does not exist.")
+    if e.response['Error']['Code'] == "403":
+      print("You do not have permission to access the S3 object you requested.")
+    else:
+      raise
+    
   # return xml file name path
-  return filePath
+  return archive_name
